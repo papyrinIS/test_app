94b99d8f6b067f0bbdd616b9ff822776d9cefc06
papyrinIS <papyrin@sumirea.ru>
2020-09-16 11:44:45 +0300

    Updates

---
+++ precache-manifest.910304f662923d808385b79ef93a859f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b2f0d1571285c671c67c6e89a71904",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "0767d2209e3bf0a5710f",
+    "url": "/test_app/static/css/main.b9596192.chunk.css"
+  },
+  {
+    "revision": "4e0458000c9ff021c63c",
+    "url": "/test_app/static/js/2.6182901c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test_app/static/js/2.6182901c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0767d2209e3bf0a5710f",
+    "url": "/test_app/static/js/main.f6d6f550.chunk.js"
+  },
+  {
+    "revision": "1ab5fdf168d6287f18bb",
+    "url": "/test_app/static/js/runtime-main.23a875dc.js"
+  }
+]);
